6e9ecea2718da98d3723fad674722332b28017cf
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-14 12:29:27 +0530

    Using vectorst to find the divisors

--- Basic Maths/printalldivisor.cpp
+++ Basic Maths/printalldivisor.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -15,5 +16,47 @@ int main()
         
     }
     
+    return 0;
+}
+*/
+
+// Another way
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Enter a positive number";
+        return 0;
+    }
+
+    vector<int> divisors;
+
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            divisors.push_back(i);
+
+            if (i != n / i)
+            {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+
+    sort(divisors.begin(), divisors.end());
+
+    for (int x : divisors)
+    {
+        cout << x << " ";
+    }
+
     return 0;
 }
